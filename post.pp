a388cc766c81527c4b068887f56ec78d65cefafc
root <root@puppetdocker-shavit.ec2.internal>
2017-03-28 14:46:07 +0000

    adding vhost to class

--- post.pp
+++ post.pp
@@ -1,4 +1,11 @@
-class { 'puppetdb': }
+class { 'puppetdb':
+#  ssl_set_cert_paths => true,
+#  ssl_deploy_certs   => true,
+  }
 class { 'puppetdb::master::config': }
-class { 'puppetexplorer': }
+
+class {'::puppetexplorer':
+  vhost_options => {
+    rewrites  => [ { rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$  https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] } ] }
+  }
 #class { 'foreman': }
